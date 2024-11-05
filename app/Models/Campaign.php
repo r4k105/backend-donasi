95eb83ab5d177c5ea848cb74174e40c596a7c127
r4k105 <ryuki0608@gmail.com>
2024-11-05 10:33:13 +0700

    Bikin Model dan migrate database

---
+++ app/Models/Campaign.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Campaign extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'category_id',
+        'target_donation',
+        'max_date',
+        'description',
+        'image',
+        'user_id',
+    ];
+}
---
+++ app/Models/Donatur.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Donatur extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'name', 
+        'email', 
+        'password', 
+        'avatar',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+}
